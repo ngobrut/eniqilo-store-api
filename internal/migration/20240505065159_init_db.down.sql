d01f79545985c831f228fb652106746b7579864d
fatih <fatih@lixus.id>
2024-05-06 20:42:01 +0700

    Update migration file

--- internal/migration/20240505065159_init_db.down.sql
+++ internal/migration/20240505065159_init_db.down.sql
@@ -1,3 +1,11 @@
-DROP INDEX IF EXISTS unique_email_idx;
+DROP INDEX IF EXISTS unique_user_phone_idx;
+DROP INDEX IF EXISTS unique_customer_phone_idx;
 
+DROP TABLE IF EXISTS checkout_products;
+DROP TABLE IF EXISTS checkouts;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS customers;
 DROP TABLE IF EXISTS users;
+
+
+
--- internal/migration/20240505065159_init_db.up.sql
+++ internal/migration/20240505065159_init_db.up.sql
@@ -1,21 +1,56 @@
 CREATE TABLE IF NOT EXISTS users (
     user_id uuid default gen_random_uuid() not null constraint users_pk primary key,
-    name varchar(255) not null,
-    email varchar(255) not null,
+    name varchar(50) not null,
+    phone varchar(25) not null,
     password varchar(500) not null,
     created_at timestamp default now(),
+    updated_at timestamp default now()
+);
+CREATE UNIQUE INDEX unique_user_phone_idx ON users (phone);
+    
+CREATE TABLE IF NOT EXISTS customers (
+    customer_id uuid default gen_random_uuid() not null constraint customers_pk primary key,
+    name varchar(50) not null,
+    phone varchar(25) not null,
+    created_at timestamp default now(),
+    updated_at timestamp default now()
+);
+CREATE UNIQUE INDEX unique_customer_phone_idx on customers (phone);
+
+CREATE TABLE IF NOT EXISTS products (
+	product_id uuid default gen_random_uuid() not null constraint products_pk primary key,
+	name varchar(30) not null,
+	sku varchar(30) not null,
+	category  varchar(30) not null,
+	image_url text not null,
+	notes varchar(200) not null,
+	price int not null,
+	stock int not null,
+	location varchar(200),
+	is_available bool not null,
+	created_at timestamp default now(),
     updated_at timestamp default now(),
     deleted_at timestamp default null
 );
 
-CREATE UNIQUE INDEX unique_email_idx ON users (email)
-WHERE
-    (deleted_at IS NULL);
+CREATE TABLE IF NOT EXISTS checkouts (
+	checkout_id uuid default gen_random_uuid() not null constraint checkouts_pk primary key,
+	customer_id uuid not null,
+	total_price int not null,
+	paid int not null,
+	change int not null,
+	created_at timestamp default now(),
+	updated_at timestamp default now(),
+	constraint customer_id_fk foreign key (customer_id) references customers(customer_id)
+);
 
-INSERT INTO
-    users(name, email, password)
-VALUES
-    (@name, @email, @password) RETURNING user_id,
-    name,
-    email,
-    created_at;
+CREATE TABLE IF NOT EXISTS checkout_products (
+	id serial not null constraint checkout_product_id_pk primary key,
+	checkout_id uuid not null,
+	product_id uuid not null,
+	quantity int not null,
+	price int not null,
+	created_at timestamp default now(),
+	constraint cp_checkout_id_fk foreign key (checkout_id) references checkouts(checkout_id),
+	constraint cp_product_id_fk foreign key (product_id) references products(product_id)
+);
