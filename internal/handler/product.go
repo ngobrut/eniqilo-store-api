3f734e5ececcbd3648c504522ff0c4e840f22e8f
fatih <fatih@lixus.id>
2024-05-10 23:25:49 +0700

    revise put product

--- internal/handler/product.go
+++ internal/handler/product.go
@@ -72,13 +72,8 @@ func (h *Handler) GetListProduct(w http.ResponseWriter, r *http.Request) {
 
 func (h *Handler) UpdateProduct(w http.ResponseWriter, r *http.Request) {
 	var req request.UpdateProduct
-	err := custom_validator.ValidateStruct(r, &req)
-	if err != nil {
-		response.Error(w, err)
-		return
-	}
 
-	req.ProductID, err = uuid.Parse(r.PathValue("id"))
+	productID, err := uuid.Parse(r.PathValue("id"))
 	if err != nil {
 		err = custom_error.SetCustomError(&custom_error.ErrorContext{
 			HTTPCode: http.StatusNotFound,
@@ -88,6 +83,13 @@ func (h *Handler) UpdateProduct(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	err = custom_validator.ValidateStruct(r, &req)
+	if err != nil {
+		response.Error(w, err)
+		return
+	}
+
+	req.ProductID = productID
 	err = h.uc.UpdateProduct(r.Context(), &req)
 	if err != nil {
 		response.Error(w, err)
