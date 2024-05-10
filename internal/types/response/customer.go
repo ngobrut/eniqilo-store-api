44249f3b6780ecaea519b4a4176dbcac791f42a6
fatih <fatih@lixus.id>
2024-05-10 22:36:31 +0700

    typo correction in type struct2

--- internal/types/response/customer.go
+++ internal/types/response/customer.go
@@ -3,13 +3,13 @@ package response
 import "github.com/google/uuid"
 
 type RegisterCustomer struct {
-	CustomerID uuid.UUID `json:"userID" db:"customer_id"`
+	CustomerID uuid.UUID `json:"userId" db:"customer_id"`
 	Phone      string    `json:"phoneNumber" db:"phone"`
 	Name       string    `json:"name" db:"name"`
 }
 
 type ListCustomer struct {
-	CustomerID uuid.UUID `json:"userID" db:"customer_id"`
+	CustomerID uuid.UUID `json:"userId" db:"customer_id"`
 	Phone      string    `json:"phoneNumber" db:"phone"`
 	Name       string    `json:"name" db:"name"`
 }
