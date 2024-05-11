e9d018846e20d28494946d9b4fb4ca00f52fad26
Fajar Pratama <fajarpratama.dev@gmail.com>
2024-05-11 15:14:31 +0700

    fix!: logger

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -23,7 +23,6 @@ func (r *customResponseWriter) WriteHeader(code int) {
 func RequestLogger(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ww := &customResponseWriter{ResponseWriter: w}
-
 		t1 := time.Now()
 
 		b, err := io.ReadAll(r.Body)
@@ -32,12 +31,21 @@ func RequestLogger(h http.Handler) http.Handler {
 		}
 
 		defer func() {
+			log.Printf("%s from %s - %d in %s \n", fmt.Sprintf("%s %s %s", r.Method, r.RequestURI, r.Proto), r.RemoteAddr, ww.statusCode, time.Since(t1).Abs().String())
+
+			fields := make(map[string]interface{})
+			token, _ := GetTokenFromHeader(r)
+			claims := ParseWithoutVerified(token)
+			if token != "" && claims != nil {
+				fields["@auth"] = map[string]interface{}{
+					"user_id": claims.UserID,
+				}
+			}
+
 			err = r.Body.Close()
 			if err != nil {
 				log.Printf("[error-body.Close()] \n%v\n", err)
 			}
-
-			fields := make(map[string]interface{})
 
 			if len(b) > 0 {
 				body := make(map[string]interface{})
@@ -47,23 +55,15 @@ func RequestLogger(h http.Handler) http.Handler {
 				}
 
 				fields["@request"] = body
-			}
 
-			token, _ := GetTokenFromHeader(r)
-			claims := ParseWithoutVerified(token)
-			if token != "" && claims != nil {
-				fields["@auth"] = map[string]interface{}{
-					"user_id": claims.UserID,
+				logfield, err := json.Marshal(fields)
+				if err != nil {
+					log.Printf("[error-json.Marshal()] \n%v\n", err)
 				}
+
+				log.Println(string(logfield))
 			}
 
-			logfield, err := json.Marshal(fields)
-			if err != nil {
-				log.Printf("[error-json.Marshal()] \n%v\n", err)
-			}
-
-			log.Printf("%s from %s - %d in %s \n", fmt.Sprintf("%s %s %s", r.Method, r.RequestURI, r.Proto), r.RemoteAddr, ww.statusCode, time.Since(t1).Abs().String())
-			log.Println(string(logfield))
 		}()
 
 		r.Body = io.NopCloser(bytes.NewBuffer(b))
