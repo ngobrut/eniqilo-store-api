0e600552f8329bc7f96dcbda7fb15421749218ea
Fajar Pratama <fajarpratama.dev@gmail.com>
2024-05-11 15:14:43 +0700

    fix!: init_db_down

--- internal/migration/20240505065159_init_db.down.sql
+++ internal/migration/20240505065159_init_db.down.sql
@@ -7,9 +7,9 @@ DROP INDEX IF EXISTS idx_invoices_customer_id;
 DROP INDEX IF EXISTS idx_invoice_products_invoice_id;
 DROP INDEX IF EXISTS idx_invoice_products_product_id;
 
-DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS invoice_products;
 DROP TABLE IF EXISTS invoices;
+DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS customers;
 DROP TABLE IF EXISTS users;
 
